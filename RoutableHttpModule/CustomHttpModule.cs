8f9af3d36d0f18c6ba148baa8f7c6ccb16989357
Martin Levington <martin.levington@gmail.com>
2018-03-24 13:45:33 +0000

    init commit

---
+++ RoutableHttpModule/CustomHttpModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace RoutableHttpModule
+{
+    public class CustomHttpModule : IHttpModule
+    {
+        public void Init(HttpApplication application)
+        {
+
+            application.BeginRequest +=  (new EventHandler(this.Application_BeginRequest));
+          
+          
+        }
+
+        private void Application_BeginRequest(Object source,EventArgs e)
+        {
+
+            HttpApplication application = (HttpApplication)source;
+            HttpContext context = application.Context;
+
+            var contextBase = context.Request.RequestContext.HttpContext;
+            if (IsRequestInRoutes(contextBase))
+            {
+                Console.WriteLine("Request Matches:"+context.Request.Url);
+            }
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsRequestInRoutes(HttpContextBase context)
+        {
+            var data = RouteTable.Routes.GetRouteData(context);
+            return data != null;
+        }
+    }
+}
---
+++ RoutableHttpModule/RouteInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace RoutableHttpModule
+{
+    public class RouteInfo
+    {
+        public RouteInfo(RouteData data)
+        {
+            RouteData = data;
+        }
+
+        public RouteInfo(Uri uri, string applicationPath)
+        {
+            RouteData = RouteTable.Routes.GetRouteData(new InternalHttpContext(uri, applicationPath));
+        }
+
+        public RouteData RouteData { get; private set; }
+
+        private class InternalHttpContext : HttpContextBase
+        {
+            private readonly HttpRequestBase _request;
+
+            public InternalHttpContext(Uri uri, string applicationPath) : base()
+            {
+                _request = new InternalRequestContext(uri, applicationPath);
+            }
+
+            public override HttpRequestBase Request => _request;
+        }
+
+        private class InternalRequestContext : HttpRequestBase
+        {
+            private readonly string _appRelativePath;
+            private readonly string _pathInfo;
+
+            public InternalRequestContext(Uri uri, string applicationPath) : base()
+            {
+                _pathInfo = ""; //uri.Query; 
+
+                if (string.IsNullOrEmpty(applicationPath) ||
+                    !uri.AbsolutePath.StartsWith(applicationPath, StringComparison.OrdinalIgnoreCase))
+                {
+                    _appRelativePath = uri.AbsolutePath;
+                }
+                else
+                {
+                    _appRelativePath = uri.AbsolutePath.Substring(applicationPath.Length);
+                }
+            }
+
+            public override string AppRelativeCurrentExecutionFilePath => string.Concat("~", _appRelativePath);
+
+            public override string PathInfo => _pathInfo;
+        }
+    }
+}
